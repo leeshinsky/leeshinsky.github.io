e40f9b5891a224166dd16321f376397ea27ce42c
leeshinsky <woosie.dhaiti@walterlcohen.org>
2024-11-22 03:56:49 +0000

    Finalized plataformer game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,8 +38,11 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
+      createPlatform(280, 300, 100, 55);
+      createPlatform(10, 400, 100, 55);
+      createPlatform(700, 390, 100, 55);
+      createPlatform(490, 460, 100, 55);
+      createPlatform(900, 500, 100, 55, "green");
 
     
     // TODO 2
@@ -47,8 +50,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("diamond", 650, 40);
+    createCollectable("database", 200, 20);
+    createCollectable("steve",1000, 70)
 
     
     // TODO 3
@@ -57,8 +61,9 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
-
+    createCannon("top", 900, 2000)
+    createCannon("bottom", 200, 1900)
+    createCannon("right",700, 2)
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
